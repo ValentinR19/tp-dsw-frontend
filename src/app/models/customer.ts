461255db38c76f06a2a11bd49450cbdd279cb466
ruciso20 <guille.petri47@gmail.com>
2025-08-26 19:57:04 -0300

    Creacion de rama dev y creacion de customer

---
+++ src/app/models/customer.ts
@@ -0,0 +1,14 @@
+export interface Customer {
+  id?: number;                // opcional cuando creás un nuevo customer
+  firstName: string;
+  lastName: string;
+  companyName: string;
+  gender?: string;             // opcional porque en backend es nullable
+  address: string;
+  zipCode: string;
+  typeOfDocument: string;
+  document: string;
+  internalCode: string;
+  active: boolean;
+  // relación con otra entidad
+}
