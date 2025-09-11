beea0b95b87b79c32f918bb4a234db8cbcd8b623
irodriguez <irodriguez.crucijuegos@gmail.com>
2025-09-11 10:42:07 -0300

    se agrega asignacion de roles al usuario

---
+++ src/app/roles/services/role.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Role } from '@main-module/app/roles/models/classes/role.entity';
+import { environment } from '@main-module/environments/environment';
+import { Observable } from 'rxjs';
+
+const ROOT = `${environment.SERVER_URL}/roles`;
+@Injectable({
+  providedIn: 'root',
+})
+export class RoleService {
+  private readonly http: HttpClient = inject(HttpClient);
+
+  findAll(): Observable<Role[]> {
+    return this.http.get<Role[]>(ROOT);
+  }
+}
