7f562814a2a6bdb5a450c84f2eb16d96e74b7c09
joaquingarciaf9 <joaquingarciaforestello@gmail.com>
2025-09-22 14:59:31 -0300

    update product-editor

--- src/app/product/models/classes/product.entity.ts
+++ src/app/product/models/classes/product.entity.ts
@@ -1,8 +1,9 @@
+import { ProductCategory } from 'src/app/product/models/classes/product-category.entity';
 export class Product {
   id?: number;
   productCategoryId?: number;
   name: string;
   description: string;
-  //productCategory: ProductCategory;
+  productCategory: ProductCategory;
   //price: ProductPrice;
   }
